8753fb48ded8787fec9d69664c2ad9f2e3ac2c16
BOSSe <BOSSe@danielnylander.se>
2026-02-11 06:55:43 +0100

    Remove xmlpatterns dependency, use native Perl XML parsing

--- oswald-scripts/check-ts-json.pl
+++ oswald-scripts/check-ts-json.pl
@@ -13,6 +13,66 @@ use POSIX 'strftime';
 use JSON::PP;
 use File::Basename;
 
+# Try XML::LibXML, fall back to regex parsing
+my $HAS_LIBXML = eval { require XML::LibXML; 1 };
+
+# Parse a .ts file and return ($translated_count, $total_count)
+sub parse_ts_file {
+    my ($file) = @_;
+    if ($HAS_LIBXML) {
+        return _parse_ts_libxml($file);
+    } else {
+        return _parse_ts_regex($file);
+    }
+}
+
+sub _parse_ts_libxml {
+    my ($file) = @_;
+    my $doc = XML::LibXML->load_xml(location => $file);
+    my $total = 0;
+    my $translated = 0;
+    for my $msg ($doc->findnodes('//message')) {
+        my ($tr) = $msg->findnodes('translation');
+        next unless $tr;
+        my $type = $tr->getAttribute('type') // '';
+        next if $type eq 'obsolete' || $type eq 'vanished';
+        $total++;
+        $translated++ unless $type eq 'unfinished';
+    }
+    return ($translated, $total);
+}
+
+sub _parse_ts_regex {
+    my ($file) = @_;
+    open my $fh, '<', $file or return (0, 0);
+    my $content = do { local $/; <$fh> };
+    close $fh;
+    my $total = 0;
+    my $translated = 0;
+    # Match each <message>...</message> block
+    while ($content =~ /<message[^>]*>(.*?)<\/message>/gs) {
+        my $block = $1;
+        if ($block =~ /<translation\s+type="(obsolete|vanished)"/) {
+            next;
+        }
+        $total++;
+        if ($block =~ /<translation\s+type="unfinished"/) {
+            # not translated
+        } else {
+            $translated++;
+        }
+    }
+    return ($translated, $total);
+}
+
+# Get percentage string for a .ts file
+sub ts_percentage {
+    my ($file) = @_;
+    my ($translated, $total) = parse_ts_file($file);
+    return "n/a" if $total == 0;
+    return int($translated * 100 / $total);
+}
+
 my %states = (
   "dev"   => "Branch is under active development. Translating it is not recommended.",
   "soft"  => "Strings are soft-frozen for the upcoming release. Some changes are still possible.",
@@ -21,9 +81,6 @@ my %states = (
   "lts"   => "Long term supported stable branch. Recommended for translation.",
   "old"   => "Old stable branch. No further releases are planned, but distributors may still pick up changes.",
 );
-
-my $qt5 = $ENV{HOME} . "/qt-l10n/qt-old515_build/qtbase";
-my $xmlpat = "LD_LIBRARY_PATH=$qt5/lib $qt5/bin/xmlpatterns";
 
 my $script_dir = dirname(__FILE__);
 my $branch_map_file = "$script_dir/branch-map.txt";
@@ -45,7 +102,8 @@ if (-f $branch_map_file) {
 }
 
 my %result_data;       # version_id => { lang => { module => pct } }
-my %result_templates;  # version_id => [ module_names ]
+my %result_templates;  # version_id => { module => template_path }
+my %result_files;      # version_id => { lang => { module => file_path } }
 my @result_versions;
 
 sub processQt {
@@ -53,16 +111,17 @@ sub processQt {
 
     my %scores;
     my %langs;
+    my %file_paths;
 
-    my $files = join("\n", <$subdir$infix/translations/*_??.ts>);
-    my $res = `$xmlpat -param files=\"$files\" check-ts.xq`;
-    for my $i (split(/[ \n]/, $res)) {
-        next unless $i =~ /^(?:[^\/]+\/)*([^.]+)\.ts:(.*)$/;
-        my ($fn, $pc) = ($1, $2);
+    my $trans_dir = "$subdir$infix/translations";
+    for my $file (glob("$trans_dir/*_??.ts")) {
+        my $bn = basename($file, ".ts");
         for my $g (@{$groups}) {
-            if ($fn =~ /^${g}_((.._)?..)$/) {
+            if ($bn =~ /^${g}_((.._)?..)$/) {
                 my $lang = $1;
+                my $pc = ts_percentage($file);
                 $scores{$g}{$lang} = $pc;
+                $file_paths{$g}{$lang} = $file;
                 $langs{$lang} = 1;
                 last;
             }
@@ -82,23 +141,27 @@ sub processQt {
     };
 
     $result_data{$id} = {};
+    $result_files{$id} = {};
     for my $lang (sort keys %langs) {
         $result_data{$id}{$lang} = {};
+        $result_files{$id}{$lang} = {};
         for my $g (@{$groups}) {
             my $pc = $scores{$g}{$lang};
             if (defined $pc) {
                 $result_data{$id}{$lang}{$g} = $pc eq "n/a" ? -1 : int($pc);
+                $result_files{$id}{$lang}{$g} = $file_paths{$g}{$lang};
             }
         }
     }
 
-    my @templates;
+    my %templates;
     for my $g (@{$groups}) {
-        if (-f $subdir . $infix . "/translations/" . $g . "_untranslated.ts") {
-            push @templates, $g;
+        my $tpl = "$trans_dir/${g}_untranslated.ts";
+        if (-f $tpl) {
+            $templates{$g} = $tpl;
         }
     }
-    $result_templates{$id} = \@templates;
+    $result_templates{$id} = \%templates;
 }
 
 sub doQt5json {
@@ -122,12 +185,14 @@ sub doOtherJson {
     my ($dirname, $filename, $appname, $type, $subdir, $version, $state) = @_;
 
     my %scores;
-    my $files = join("\n", <$subdir/$dirname/${filename}*_??.ts>);
-    my $res = `$xmlpat -param files=\"$files\" check-ts.xq`;
-    for my $i (split(/ /, $res)) {
-        if ($i =~ /^(?:[^\/]+\/)*${filename}_(.*)\.ts:(.*)$/) {
-            my ($lang, $pc) = ($1, $2);
-            $scores{$lang} = $pc;
+    my %file_paths;
+    my $trans_dir = "$subdir/$dirname";
+    for my $file (glob("$trans_dir/${filename}_??.ts"), glob("$trans_dir/${filename}_??_??.ts")) {
+        if (basename($file) =~ /^${filename}_(.+)\.ts$/) {
+            my $lang = $1;
+            next if $lang eq 'untranslated';
+            $scores{$lang} = ts_percentage($file);
+            $file_paths{$lang} = $file;
         }
     }
 
@@ -144,18 +209,23 @@ sub doOtherJson {
     };
 
     $result_data{$id} = {};
+    $result_files{$id} = {};
     for my $lang (sort keys %scores) {
         my $pc = $scores{$lang};
         $result_data{$id}{$lang} = {
             $filename => ($pc eq "n/a" ? -1 : int($pc)),
         };
+        $result_files{$id}{$lang} = {
+            $filename => $file_paths{$lang},
+        };
     }
 
-    my @templates;
-    if (-f "$subdir/$dirname/${filename}_untranslated.ts") {
-        push @templates, $filename;
+    my %templates;
+    my $tpl = "$trans_dir/${filename}_untranslated.ts";
+    if (-f $tpl) {
+        $templates{$filename} = $tpl;
     }
-    $result_templates{$id} = \@templates;
+    $result_templates{$id} = \%templates;
 }
 
 sub doCreatorJson { doOtherJson("share/qtcreator/translations", "qtcreator", "Qt Creator", "creator", @_); }
@@ -183,6 +253,7 @@ my %output = (
     generated  => strftime("%Y-%m-%dT%H:%M:%SZ", gmtime(time)),
     versions   => \@result_versions,
     data       => \%result_data,
+    files      => \%result_files,
     templates  => \%result_templates,
 );
 
